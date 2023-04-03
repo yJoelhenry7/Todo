ca73e4926a8794c9eb1b473920f87df0c2dd6f6d
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2023-04-03 12:53:58 +0530

    Add csrf

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,7 +1,12 @@
 const db = require("../models/index");
 const app = require("../app");
 const request = require("supertest");
+const cheerio = require("cheerio");
 let server, agent;
+function extractCsrfToken(res) {
+  var $ = cheerio.load(res.text);
+  return $("[name=_csrf]").val();
+}
 describe("List the todo items", function () {
     beforeAll(async () => {
       await db.sequelize.sync({ force: true });
@@ -14,13 +19,42 @@ describe("List the todo items", function () {
     });
     // add todo test
     test("create a todo", async () => {
+      const res = await agent.get("/");
+      const csrfToken = extractCsrfToken(res);
       const response = await agent.post("/todos").send({
         title: "Buy milk",
         dueDate: new Date().toISOString(),
         completed: false,
+        "_csrf": csrfToken,
       });
       expect(response.statusCode).toBe(302);
     });
-
+  // mark todo as complete test
+  test("Mark a todo as complete", async () => {
+    let res = await agent.get("/");
+    let csrfToken = extractCsrfToken(res);
+    await agent.post("/todos").send({
+      title: "Buy milk",
+      dueDate: new Date().toISOString(),
+      completed: false,
+      "_csrf": csrfToken,
+    });
+    const groupedTodosResponse = await agent
+      .get("/")
+      .set("Accept", "application/json");
+    const parsedGroupedResponse = JSON.parse(groupedTodosResponse.text);
+    const dueTodayCount = parsedGroupedResponse.DT.length;
+    const latestTodo = parsedGroupedResponse.DT[dueTodayCount - 1];
+    
+    res = await agent.get("/");
+    csrfToken = extractCsrfToken(res);
+    const markCompleteResponse = await agent
+    .put(`/todos/${latestTodo.id}/markAsCompleted`)
+    .send({
+     "_csrf": csrfToken,
+    });
+    const parsedUpdateResponse = JSON.parse(markCompleteResponse.text);
+    expect(parsedUpdateResponse.completed).toBe(true);
+  });
   });
   
