b60ab5a64073084f4c505b3ba457e7656e89f630
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2023-04-04 15:55:22 +0530

    user Authentication using Passport

--- app.js
+++ app.js
@@ -2,24 +2,77 @@ const { response } = require("express");
 const express = require("express");
 const { request } = require("http");
 const app = express();
-const { Todo,User } = require("./models");
 const bodyParser = require("body-parser");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 const path = require("path");
-app.use(bodyParser.json());
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended:false}));
 var date = new Date();
 var rdate = date.toISOString().split('T')[0];
 const csrf = require("tiny-csrf");
 const cookieParser = require("cookie-parser");
 const e = require("express");
+const passport = require('passport');
+const connectEnsureLogin = require('connect-ensure-login');
+const session = require('express-session');
+const localStrategy = require('passport-local');
+const { error } = require("console");
+
+// set ejs as a View Engine
+app.set("view engine", "ejs");
+// middlewares
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended:false}));
 app.use(cookieParser("ssh! some secret string"));
 app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
+app.use(session({
+  secret:"my-super-secret-key-21728172615261562",
+  cookie:{
+    maxAge:24 * 60 * 60 * 1000 //24 hours
+  }
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
+// Authentication Strategy for passport
+passport.use(new localStrategy({
+   usernameField: 'email',
+   passwordField:'password'
+}, (username,password,done)=>{
+  User.findOne({where: {email:username,password:password}})
+  .then((user)=>{
+    return done(null,user)
+  }).catch((error)=>{
+    return (error)
+  })
+}))
+// Sereializing user information
+passport.serializeUser((user,done)=>{
+  console.log("Serializing user in session", user.id)
+  done(null,user.id)
+})
+// deserializing user information
+passport.deserializeUser((id,done)=>{
+  User.findByPk(id)
+  .then(user =>{
+    done(null,user)
+  })
+  .catch((error)=>{
+    done(error,null)
+  })
+})
+
+const { Todo,User } = require("./models");
+
+// ports
 app.get("/", async (request, response) => {
+   response.render("index",{
+    title:"Todo Application",
+    csrfToken:request.csrfToken(),
+   });
+});
+
+app.get("/todos",connectEnsureLogin.ensureLoggedIn(), async (request, response) => {
   const complete = await Todo.getCompleted();
   Todo.findAll().then((todos) => {
     var overDue = [];
@@ -35,7 +88,7 @@ app.get("/", async (request, response) => {
       }
     });
     if (request.accepts("html")) {
-      response.render("index", {
+      response.render("todos", {
         OD: overDue,
         DL: dueLater,
         DT: dueToday,
@@ -51,40 +104,6 @@ app.get("/", async (request, response) => {
     }
   });
 });
-
-app.get("/todos", async (request, response) => {
-  console.log("Todo List");
-  try {
-    const todo = await Todo.findAll();
-    return response.json(todo);
-  } catch (error) {
-    console.log(error);
-    return response.status(422).json(error);
-  }
-});
-
-app.get("/signup",(request,response) =>{
-  response.render("signup",{
-    csrfToken: request.csrfToken(),
-  });
-})
-app.post("/users",async(request,response)=>{
-   console.log(request.body.firstName)
-  //  Have to create the user 
-  try {
-    const user = await User.create({
-      firstName : request.body.firstName,
-      lastName : request.body.lastName,
-      email:request.body.email,
-      password:request.body.password,
-    });
-    response.redirect("/");
-  } catch (error) {
-    console.log(error);
-  }
-
-})
-
 app.post("/todos", async (request, response) => {
   // console.log("Creating a Todo", request.body);
   try {
@@ -119,5 +138,33 @@ app.delete("/todos/:id", async(request, response) => {
     return response.status(422).json(error);
   }
 });
+
+// -----------------------------------------------------------------
+app.get("/signup",(request,response) =>{
+  response.render("signup",{
+    csrfToken: request.csrfToken(),
+  });
+})
+app.post("/users",async(request,response)=>{
+   console.log(request.body.firstName)
+  //  Have to create the user 
+  try {
+    const user = await User.create({
+      firstName : request.body.firstName,
+      lastName : request.body.lastName,
+      email:request.body.email,
+      password:request.body.password,
+    });
+    request.logIn(user,(err)=>{
+      if(err){
+        console.log(err);
+      }
+      response.redirect("/todos");
+    })
+  } catch (error) {
+    console.log(error);
+  }
+})
+// --------------------------------------------------------------------------------------
 
 module.exports = app;
